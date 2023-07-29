0088dffff2481a0eedd1581292cd0bce401eed37
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-29 17:45:53 -0400

    Depend on older Log4J for 1.7

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -25,8 +25,8 @@ tasks.processResources.configure {
 }
 
 dependencies {
-    compileOnly("org.apache.logging.log4j:log4j-api:2.15.0")
-    compileOnly("org.apache.logging.log4j:log4j-core:2.15.0")
+    compileOnly("org.apache.logging.log4j:log4j-api:2.0-beta9")
+    compileOnly("org.apache.logging.log4j:log4j-core:2.0-beta9")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
