921aa7b592af14aa97e204320280dd5ce9f563de
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-29 11:56:21 -0400

    Initial commit

---
+++ src/main/java/info/mmpa/pipeblocker/PipeBlocker.java
@@ -0,0 +1,18 @@
+package info.mmpa.pipeblocker;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "pipeblocker", name = "PipeBlocker", version = Tags.VERSION)
+public class PipeBlocker {
+
+    public static final Logger LOGGER = LogManager.getLogger("PipeBlocker");
+
+    @Mod.EventHandler
+    @SuppressWarnings("unused")
+    public void init(FMLInitializationEvent ev) {
+        LOGGER.info("Pipes being blocked.");
+    }
+}
