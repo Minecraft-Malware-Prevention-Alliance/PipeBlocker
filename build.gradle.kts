805917dd456d9b163b2bade36d5931e0c3430ba9
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-29 15:07:31 -0400

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 // Project properties
 group = "info.mmpa.pipeblocker"
-version = "1.0.0"
+version = "1.1.0"
 
 // Set the toolchain version to decouple the Java we run Gradle with from the Java used to compile and run the mod
 java {
