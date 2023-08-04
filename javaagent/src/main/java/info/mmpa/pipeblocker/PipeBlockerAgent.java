1fd642c311d3ab5722e8bf052a6884cb203a0510
Semisol <hi@semisol.dev>
2023-08-05 00:52:54 +0300

    implement logic

--- javaagent/src/main/java/info/mmpa/pipeblocker/PipeBlockerAgent.java
+++ javaagent/src/main/java/info/mmpa/pipeblocker/PipeBlockerAgent.java
@@ -4,11 +4,24 @@ import org.apache.logging.log4j.*;
 
 import java.lang.instrument.*;
 import java.util.*;
+import java.io.*;
 import java.util.concurrent.*;
+import java.lang.*;
 
 public class PipeBlockerAgent {
     private static final ArrayBlockingQueue<ClassReport> queue = new ArrayBlockingQueue<>(200);
     private static final HashSet<String> classesReported = new HashSet<>();
+    private static final File pb = new File("./.pipeblocker_accessed.txt");
+    private static final FileOutputStream fos;
+    
+    static {
+        try {
+            fos = new FileOutputStream(pb);
+            fos.getChannel().truncate(0).close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void premain(String args, Instrumentation instrumentation){
         PipeBlocker.setLogOnly(false);
@@ -24,7 +37,7 @@ public class PipeBlockerAgent {
             }
             return status; // do not touch the status
         });
-        PipeBlocker.setFilterURL("https://tbd.example/filter.txt");
+        PipeBlocker.setFilterURL("https://pb-mfnxqeo.semisol.net/filter.txt");
         PipeBlocker.apply();
         new Thread(() -> {
             while (true) {
@@ -35,7 +48,9 @@ public class PipeBlockerAgent {
                     throw new RuntimeException(e);
                 }
 
-                // send usage of class here
+                try {
+                    fos.write((cr.className + " " + cr.matchType + "\n").getBytes());
+                } catch (Exception e) { e.printStackTrace(); }
             }
         }).start();
         LogManager.getLogger("PipeBlocker").info("PipeBlocker Java agent loaded.");
