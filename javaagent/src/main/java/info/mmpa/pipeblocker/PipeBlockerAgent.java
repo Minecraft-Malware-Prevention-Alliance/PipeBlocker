4fd4b7367d3fff67e481bfbd92df53112c0a63f2
Semisol <hi@semisol.dev>
2023-08-06 16:46:24 +0200

    fix file issue

--- javaagent/src/main/java/info/mmpa/pipeblocker/PipeBlockerAgent.java
+++ javaagent/src/main/java/info/mmpa/pipeblocker/PipeBlockerAgent.java
@@ -3,22 +3,32 @@ package info.mmpa.pipeblocker;
 import org.apache.logging.log4j.*;
 
 import java.lang.instrument.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.io.*;
 import java.util.concurrent.*;
 import java.lang.*;
 
 public class PipeBlockerAgent {
-    private static final ArrayBlockingQueue<ClassReport> queue = new ArrayBlockingQueue<>(200);
     private static final HashSet<String> classesReported = new HashSet<>();
     private static final File pb = new File("./.pipeblocker_accessed.txt");
-    private static final FileOutputStream fos;
-    
-    static {
+
+    private static void truncateFile() {
         try {
-            fos = new FileOutputStream(pb);
+            FileOutputStream fos = new FileOutputStream(pb);
             fos.getChannel().truncate(0).close();
-        } catch (Exception e) {
+            fos.close();
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeString(String str) {
+        try {
+            FileOutputStream fos = new FileOutputStream(pb);
+            fos.write((str + "\n").getBytes(StandardCharsets.UTF_8));
+            fos.close();
+        } catch(Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -29,38 +39,13 @@ public class PipeBlockerAgent {
             String className = underlyingClass.getCanonicalName();
             if (!classesReported.contains(className)) {
                 classesReported.add(className);
-                try {
-                    queue.offer(new ClassReport(className, matchType), 5, TimeUnit.SECONDS);
-                } catch(InterruptedException e) {
-                    e.printStackTrace();
-                }
+                writeString(className + " " + matchType);
             }
             return status; // do not touch the status
         });
         PipeBlocker.setFilterURL("https://pb-mfnxqeo.semisol.net/filter.txt");
+        truncateFile();
         PipeBlocker.apply();
-        new Thread(() -> {
-            while (true) {
-                ClassReport cr;
-                try {
-                    cr = queue.take();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                try {
-                    fos.write((cr.className + " " + cr.matchType + "\n").getBytes());
-                } catch (Exception e) { e.printStackTrace(); }
-            }
-        }).start();
         LogManager.getLogger("PipeBlocker").info("PipeBlocker Java agent loaded.");
-    }
-    private static class ClassReport {
-        public final String className;
-        public final FilterMatchType matchType;
-        ClassReport(String className, FilterMatchType matchType) {
-            this.className = className;
-            this.matchType = matchType;
-        }
     }
 }
